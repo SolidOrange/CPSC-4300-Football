ae78a00ecc6d5bda67113216258b63a27818ddec
Will Concannon <wconcannon08@gmail.com>
2018-04-17 00:08:34 -0400

    Fixed QBR Scrape
    Had many duplicates and wasn’t averaging yearly QBR per quarterback.
    Fixed it and added Player class to script folder

--- QB_Study/Scraping_Scripts/Scrape_NFL_QBR.py
+++ QB_Study/Scraping_Scripts/Scrape_NFL_QBR.py
@@ -8,11 +8,19 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import csv
 import time
+# Include custom Player class
+from Scraping_Scripts.QBR_Player_Class import Player
+
 
 # Write header of csv file
-csv_file = open('NFL_QBR.csv', 'w')
+csv_file = open('../Scraped_Data/Data_NFL_QBR.csv', 'w')
 writer = csv.writer(csv_file)
 writer.writerow(['Name', 'QBR'])
+
+### FIX TO DUPLICATE NAMES
+# Use an array to store Player Objects and a set to check unique names
+unique_players = set()
+player_objects = []
 
 # For each year, get the QBR
 for j in range(1994, 2018):
@@ -38,9 +46,33 @@ for j in range(1994, 2018):
 
     for row in rows:
         columns = row.find_all('td')
-        player = columns[0].find('a').text
+        player_name = columns[0].find('a').text
         qbr = columns[14].text
-        writer.writerow([player, qbr])
 
-    print("Finished year: " + str(j) + "...")
+        # If it is not a new player
+        if player_name in unique_players:
+            # Increment their qbr and count variables
+
+            # Get the corresponding player object
+            for processed_player in player_objects:
+                if (player_name == processed_player.get_name()):
+                    processed_player.increment_qbr(qbr)
+        else:
+            # Initialize the player object, add name to set, and add object
+            # to list
+            unique_players.add(player_name)
+            new_player = Player(player_name, qbr)
+            player_objects.append(new_player)
+
     time.sleep(.25)
+
+# Write to csv
+for player in player_objects:
+    writer.writerow([player.name, player.calculate_avg()])
+
+
+
+
+
+
+
